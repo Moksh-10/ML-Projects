1c6c39f1f3a99e78bdb89fc840df7373690b3d56
Moksh-10 <agmoksh10@gmail.com>
2025-12-22 11:24:31 +0530

    old stuff

--- GAN/Simple-GAN/model.py
+++ GAN/Simple-GAN/model.py
@@ -1,102 +1,156 @@
 import torch
 from torch import nn
-import torchvision
-import torchvision.datasets as datasets
+import numpy as np
+from torchvision.datasets import MNIST
+from torchvision import transforms
 from torch.utils.data import DataLoader
-import torchvision.transforms as transforms
-from torch.utils.tensorboard import SummaryWriter
+import torch.optim as optim
+import matplotlib.pyplot as plt  
+from tqdm import tqdm
+
 
 class discriminator(nn.Module):
-    def __init__(self, img_dim):
+    def __init__(self):
         super().__init__()
-        in_f = img_dim
         self.disc = nn.Sequential(
-            nn.Linear(in_f, 128),
-            nn.LeakyReLU(0.1),
-            nn.Linear(128, 1),
-            nn.Sigmoid()
-        )
-
+                nn.Linear(784, 1024),
+                nn.LeakyReLU(), # performs better than ReLU
+                nn.Dropout(p=0.2),
+                nn.Linear(1024, 512),
+                nn.LeakyReLU(), 
+                nn.Dropout(p=0.2),
+                nn.Linear(512, 256),
+                nn.LeakyReLU(), 
+                nn.Dropout(p=0.2),
+                nn.Linear(256, 1)
+                )
+    
     def forward(self, x):
+        bs = x.shape[0]
+        x = x.reshape(bs, -1)
         return self.disc(x)
 
-class gen(nn.Module):
-    def __init__(self, z_dim, img_dim):
+
+class generator(nn.Module):
+    def __init__(self, latent_dim):
         super().__init__()
         self.gen = nn.Sequential(
-            nn.Linear(z_dim, 256),
-            nn.LeakyReLU(0.1),
-            nn.Linear(256, img_dim),
-            nn.Tanh()
-        )
+                nn.Linear(latent_dim, 256),
+                nn.LeakyReLU(),
+                nn.Dropout(p=0.2),
+                nn.Linear(256, 512),
+                nn.LeakyReLU(),
+                nn.Dropout(p=0.2),
+                nn.Linear(512, 1024),
+                nn.LeakyReLU(),
+                nn.Dropout(p=0.2),
+                nn.Linear(1024, 784),
+                nn.Tanh()
+                )
+    
+    def forward(self, noise):
+        bs = noise.shape[0]
+        ge = self.gen(noise)
+        ge = ge.reshape(bs, 1, 28, 28)
+        return ge
 
-    def forward(self, x):
-        return self.gen(x)
 
-
+latent_dim = 100
+batch_size = 64
 device = "cuda" if torch.cuda.is_available() else "cpu"
-lr = 3e-4
-z_dim = 64
-img_dim = 28 * 28 *1
-bs = 32
-ne = 50
 
-disc = discriminator(img_dim).to(device)
-gen = gen(z_dim, img_dim).to(device)
-fixed_noise = torch.randn((bs, z_dim)).to(device)
+gen_lr = 0.0001
+disc_lr = 0.0001
 
-tr = transforms.Compose([
-    transforms.ToTensor(),
-    transforms.Normalize((0.5,), (0.5,))
-])
-dataset = datasets.MNIST(root="/dataset", transform=tr, download=True)
-loader = DataLoader(dataset, bs, shuffle=True)
-opt_disc = torch.optim.Adam(disc.parameters(), lr)
-opt_gen = torch.optim.Adam(gen.parameters(), lr)
-loss_fn = nn.BCELoss()
-writer_fake = SummaryWriter(f"runs/GAN_MNIST/fake")
-writer_real = SummaryWriter(f"runs/GAN_MNIST/real")
-step = 0
+gen = generator(latent_dim=latent_dim).to(device)
+disc = discriminator().to(device)
 
-for e in range(ne):
-    for batch_idx, (real, _) in enumerate(loader):
-        real = real.view(-1, 784).to(device)
-        bs = real.shape[0]
+gen_optim = optim.Adam(gen.parameters(), gen_lr)
+disc_optim = optim.Adam(disc.parameters(), disc_lr)
 
-        # train discriminator: max log(D(real)) + log(1 - D(G(z)))
-        noise = torch.randn(bs, z_dim).to(device)
-        fake = gen(noise)
-        disc_real = disc(real).view(-1)
-        lossD_real = loss_fn(disc_real, torch.ones_like(disc_real))  # y log x + (1 - y) log (1 - x) -> y = 1 -> log x
-        disc_fake = disc(fake).view(-1) # D(G(z))
-        lossD_fake = loss_fn(disc_fake, torch.zeros_like(disc_fake)) # y log x + (1 - y) log (1 - x) -> y = 0 -> log (1 - x)
-        lossD = (lossD_fake + lossD_real) / 2
+t2i = transforms.Compose(
+        [
+            transforms.ToTensor(),
+            transforms.Normalize((0.5, ), (0.5, ))
+            ])
 
-        opt_disc.zero_grad()
-        lossD.backward(retain_graph=True)
-        opt_disc.step()
+tr = MNIST("./data/", transform=t2i, download=True)
+tl = DataLoader(tr, batch_size=batch_size, shuffle=True)
 
-        # train generator min log(1 - D(G(z))) <-> max log(D(G(z)))
-        out = disc(fake).view(-1)
-        lossG = loss_fn(out, torch.ones_like(out))
-        opt_gen.zero_grad()
-        lossG.backward()
-        opt_gen.step()
+def train_gan(gen, disc, gen_optim, disc_optim, tl, epochs=200, device="cpu", plot_gen_freq=50, plot_loss_freq=20, num_gens=10):
 
-        if batch_idx == 0:
-            print(
-                f"epoch [{e}/{ne}] \ "
-                f"loss D: {lossD:.4f}, loss G: {lossG:.4f}"
-            )
+    loss_fn = nn.BCEWithLogitsLoss() # as disc gives raw logit
+    gen_losses, disc_losses = [], []
 
+    for e in tqdm(range(epochs)):
+
+        gen_epoch_losses = []
+        disc_epoch_losses = []
+
+        for im, _ in tl:
+            # b, c, h, w
+            bs = im.shape[0]
+            im = im.to(device)
+
+            #train disc
+            noise = torch.randn(bs, latent_dim, device=device)
+
+            gen_labels = torch.zeros(bs, 1, device=device)
+            true_labels = torch.ones(bs, 1, device=device)
+
+            gen_im = gen(noise).detach() # gradients won't go through into the generator
+
+            real_disc_pred = disc(im)
+            gen_disc_pred = disc(gen_im)
+
+            real_loss = loss_fn(real_disc_pred, true_labels)
+            fake_loss = loss_fn(gen_disc_pred, gen_labels)
+            disc_loss = (real_loss + fake_loss) / 2
+            disc_epoch_losses.append(disc_loss.item())
+
+            disc_optim.zero_grad()
+            disc_loss.backward()
+            disc_optim.step()
+
+            # train gen
+            noise = torch.randn(bs, latent_dim, device=device)
+            gen_im = gen(noise)
+            gen_disc_pred = disc(gen_im)
+
+            gen_loss = loss_fn(gen_disc_pred, true_labels)
+            gen_epoch_losses.append(gen_loss.item())
+
+            gen_optim.zero_grad()
+            gen_loss.backward()
+            gen_optim.step()
+
+        gen_epoch_losses = np.mean(gen_epoch_losses)
+        disc_epoch_losses = np.mean(disc_epoch_losses)
+
+        if e % plot_loss_freq == 0:
+            print(f"epoch: {e}/{epochs} | gen loss: {gen_epoch_losses} | disc_loss: {disc_epoch_losses}")
+
+        if e % plot_gen_freq == 0:
+            gen.eval()
             with torch.no_grad():
-                fake = gen(fixed_noise).reshape(-1, 1, 28, 28)
-                data = real.reshape(-1, 1, 28, 28)
-                img_grid_fake = torchvision.utils.make_grid(fake, normalize=True)
-                img_grid_real = torchvision.utils.make_grid(real, normalize=True)
+                noise_sample = torch.randn(num_gens, latent_dim, device=device)
+                gen_imgs = gen(noise_sample).to("cpu")
 
-                writer_fake.add_image("mnist fake", img_grid_fake, global_step=step)
-                writer_real.add_image("mnist real", img_grid_real, global_step=step)
+                fig, ax = plt.subplots(1, num_gens, figsize=(15, 5))
+                for i in range(num_gens):
+                    img = (gen_imgs[i].squeeze() + 1) / 2 # b , 1, 28, 28
+                    ax[i].imshow(img.numpy(), cmap="gray")
+                    ax[i].set_axis_off()
+                plt.show()
+            gen.train()
+    return gen, disc, gen_losses, disc_losses
 
-                step += 1
+
+gener, discr, gen_l, disc_l = train_gan(gen, disc, gen_optim, disc_optim, tl, epochs=200, device="cuda", plot_gen_freq=40, plot_loss_freq=40)
+
+
+plt.plot(gen_l, label="gen loss")
+plt.plot(disc_l, label="disc_l")
+plt.legend()
+plt.show()
 
